1e9a6ae443d312154a6cee1e4858738f74086ac0
Teomayo <tode.crnobrnja@gmail.com>
2024-12-27 13:17:03 -0500

    non logged in users are bounced to login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Settings from "./components/Settings";
 import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
 import "./styles/App.css";
 
 const App: React.FC = () => {
@@ -10,11 +11,24 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <PrivateRoute>
+              <Settings />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
 };
-
 export default App;
